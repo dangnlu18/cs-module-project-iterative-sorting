6b8af113469ddd9f6c10bb2d4b12129d3a230048
dang lu <danglu@dangs-mbp.lan>
2020-07-08 20:37:00 -0700

    selection and bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,14 +3,18 @@ def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[cur_index]:
+                cur_index = j
 
         # TO-DO: swap
         # Your code here
+
+        if cur_index != i:
+            arr[cur_index], arr[i] = arr[i], arr[cur_index]
 
     return arr
 
@@ -18,6 +22,14 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                sorted = False
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+
 
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,10 @@
+from math import floor
+
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -10,5 +14,17 @@ def binary_search(arr, target):
 
     # Your code here
 
-
-    return -1  # not found
+    start_index = 0
+    end_index= len(arr)-1
+    found = False
+    while not found:
+        mid = floor((start_index + end_index)/2)
+        if arr[mid] == target:
+            found = True
+            return mid
+        else:
+            if arr[mid] > target:
+                end_index = mid-1
+            if arr[mid] < target:
+                start_index = mid+1
+        return -1  # not found
